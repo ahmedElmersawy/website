1cc5d336a070d4ad657199d39a4fc0da20fe0c04
Ahmed Elmersawy <ahmedelmersawy@Ahmeds-MacBook-Pro.local>
2026-07-02 12:51:35 -0400

    Remove 'expected' from graduation date

--- src/content/resume.ts
+++ src/content/resume.ts
@@ -19,7 +19,7 @@ export const education: EducationEntry[] = [
     degree: "Bachelor of Science in Electrical and Computer Engineering",
     institution: "Purdue University",
     location: "West Lafayette, IN",
-    date: "2023 - 2026 (expected)",
+    date: "2023 - 2026",
     details: [
       "GPA: 3.77.",
       "Graduate-level coursework: Artificial Intelligence, Machine Learning, Data Mining, Advanced Computer Networks, Applied Cryptography, Embedded Systems.",
